f171403f60b17e1bece4de6d9d02d261bbe26657
Wojciech Karas <wkaras89@gmail.com>
2017-12-30 18:32:36 +0100

    implement map, filter, some, every, find

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,33 @@
-import { add } from "./index"
+import { map, filter, reduce, some, every, find } from "./index"
 
-test("add", () => {
-  expect(add(1, 2)).toBe(3)
+const arr = [1, 2, 3]
+
+test("map", () => {
+  expect(map((x) => x * 2, arr)).toEqual([2, 4, 6])
+  expect(map<number, number>((x) => x * 2)(arr)).toEqual([2, 4, 6])
+})
+
+test("filter", () => {
+  expect(filter((x) => x > 1, arr)).toEqual([2, 3])
+  expect(filter<number>((x) => x > 1)(arr)).toEqual([2, 3])
+})
+
+test("reduce", () {
+  expect(reduce((acc, v) => acc + v, 0 ,arr)).toEqual(6);
+  expect(reduce<number, number>((acc, v) => acc + v, 0)(arr)).toEqual(6);
+})
+
+test("some", () => {
+  expect(some(x => x > 3,  arr)).toEqual(false)
+  expect(some<number>(x => x > 2)(arr)).toEqual(true)
+})
+
+test("every", () => {
+  expect(every(x => x < 10, arr)).toEqual(true)
+  expect(every<number>(x => x < 3)(arr)).toEqual(false)
+})
+
+test("find", () => {
+  expect(find(x => x < 3, arr)).toEqual(1);
+  expect(find<number>(x => x === 5, arr)).toEqual(undefined);
 })
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,55 @@
-export const add = (a: number, b: number) => a + b
+import { curry2Last, curry3Last } from "ts-function"
 
-console.log(add(1, 2))
+type Transform<T, R> = (x: T, i: number) => R
+type Predicate<T> = (x: T, i: number) => boolean
+type Reducer<Acc, T> = (acc: Acc, x: T, i: number) => Acc
+
+type Map = {
+  <T, R>(map: Transform<T, R>, arr: T[]): R[]
+  <T, R>(map: Transform<T, R>): (arr: T[]) => R[]
+}
+export const map = curry2Last(<T, R>(map: Transform<T, R>, arr: T[]): R[] =>
+  arr.map(map),
+) as Map
+
+type Filter = {
+  <T>(predicate: Predicate<T>, arr: T[]): T[]
+  <T>(predicate: Predicate<T>): (arr: T[]) => T[]
+}
+export const filter = curry2Last(<T>(predicate: Predicate<T>, arr: T[]): T[] =>
+  arr.filter(predicate),
+) as Filter
+
+type Reduce = {
+  <T, Acc>(reducer: Reducer<Acc, T>, init: Acc, arr: T[]): Acc
+  <T, Acc>(reducer: Reducer<Acc, T>, init: Acc): (arr: T[]) => Acc
+}
+export const reduce = curry3Last(
+  <T, Acc>(reducer: Reducer<Acc, T>, init: Acc, arr: T[]): Acc =>
+    arr.reduce(reducer, init),
+) as Reduce
+
+type ArrToBoolean = {
+  <T>(predicate: Predicate<T>, arr: T[]): boolean
+  <T>(predicate: Predicate<T>): (arr: T[]) => boolean
+}
+export const some = curry2Last(
+  <T>(predicate: Predicate<T>, arr: T[]): boolean => arr.some(predicate),
+) as ArrToBoolean
+
+export const every = curry2Last(
+  <T>(predicate: Predicate<T>, arr: T[]): boolean => arr.every(predicate),
+) as ArrToBoolean
+
+type Find = {
+  <T>(predicate: Predicate<T>, arr: T[]): T | undefined
+  <T>(predicate: Predicate<T>): (arr: T[]) => T | undefined
+}
+export const find = curry2Last(<T>(predicate: Predicate<T>, arr: T[]):
+  | T
+  | undefined => {
+  for (let i = 0; i < arr.length; i++) {
+    if (predicate(arr[i], i)) return arr[i]
+  }
+  return undefined
+}) as Find
